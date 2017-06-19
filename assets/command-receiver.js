d7b748798b38fdaa63019ad15396c3e3caee78e7
Suhun Kim <cobject@gmail.com>
2017-06-19 00:55:01 -0400

    Add control packet

--- assets/command-receiver.js
+++ assets/command-receiver.js
@@ -8,24 +8,29 @@ console.log("host = ", HOST);
 
 client.connect(3002, HOST, function() {
 	console.log('Connected');
+	var buf = new Uint8Array([1, 0]);;
   $('#robot-control-left').click(function(){
-    client.write("left");
+    buf[1] = 1;
+    client.write(Buffer.from(buf));
   });
   $('#robot-control-go').click(function(){
-    client.write("go");
+    buf[1] = 2;
+    client.write(Buffer.from(buf));
   });
   $('#robot-control-back').click(function(){
-    client.write("back");
+    buf[1] = 3;
+    client.write(Buffer.from(buf));
   });
   $('#robot-control-right').click(function(){
-    client.write("right");
+    buf[1] = 4;
+    client.write(Buffer.from(buf));
   });
 });
 
 client.on('data', onData);
 
 function onData(data){
-  console.log('Received');
+  console.log('Received: ', data);
   if(data == "left ok") {
     console.log("left ok");
   } else if(data == "right ok") {
